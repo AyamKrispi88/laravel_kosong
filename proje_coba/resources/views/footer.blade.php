5a1ed48ff6f200d7a856104637f2f175962c2b5d
Krishna Ardi <baguskrishna52@gmail.com>
2026-02-11 17:40:57 +0800

    Futer

---
+++ proje_coba/resources/views/footer.blade.php
@@ -0,0 +1,18 @@
+<nav class="bg-sinfo-white px-4 sm:px-8 py-4 fixed border-[1.27px] border-gray-300 flex justify-between items-center w-full z-9999">
+    <a href="/">
+        <h1 class="text-lg sm:text-2xl text-primary font-semibold">SIC Info</h1>
+    </a>
+    <div class="hidden sm:flex gap-5">
+        <x-nav-link href="/info-lomba">Info Lomba</x-nav-link>
+        <x-nav-link href="/">Info Tempat Makan</x-nav-link>
+        <x-nav-link href="/">Info Loker</x-nav-link>
+    </div>
+    <div class="flex items-center gap-3">
+        <a href="">
+            <x-button size="sm">Login</x-button>
+        </a>
+        <button class="block sm:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" class="size-4 fill-sinfo-black" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z"/></svg>
+        </button>
+    </div>
+</nav>
